7cbb5b127caed6925ebc657d558a17a3926cfcd5
Miu <miyuuu0728@gmail.com>
2021-08-17 16:15:45 -0700

    added meta

--- src/app/store.js
+++ src/app/store.js
@@ -1,21 +1,6 @@
-// import { configureStore } from "@reduxjs/toolkit";
-// import { persistStore } from "redux-persist";
-// import thunk from "redux-thunk";
-
 import userSlice from "../features/userSlice";
 import itemSlice from "../features/itemSlice";
 import cartSlice from "../features/cartSlice";
-
-// export const store = configureStore({
-//   reducer: {
-//     user: userSlice,
-//     item: itemSlice,
-//     cart: cartSlice,
-//   },
-//   middleware: [thunk],
-// });
-
-// export const persistor = persistStore(store);
 
 import { configureStore } from "@reduxjs/toolkit";
 import storage from "redux-persist/lib/storage";
@@ -38,7 +23,6 @@ const persistedReducer = persistReducer(persistConfig, reducers);
 
 const store = configureStore({
   reducer: persistedReducer,
-  // devTools: process.env.NODE_ENV !== "production",
   middleware: [thunk],
 });
 
